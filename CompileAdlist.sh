d64907618c6b20e63da807e708f78e1485ddae02
RC62 <RC62@users.noreply.github.com>
2022-03-07 13:52:49 -0500

    x

---
+++ CompileAdlist.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script for use on PiHole RPI
+
+# Compile adlist files
+hostlist-compiler -c configuration.json -o output.txt
+sleep 1
+
+# Cleanup IPs
+sed -i '' "s/127.0.0.1//g" output.txt
+sleep 1
+
+sed -i '' "s/0.0.0.0//g" output.txt
+sleep 1
+
+# Cleanup comments - replace "!" with "#"
+sed -i '' "s/\!/\#/g" output.txt
+sleep 1
+
+# Remove all empty lines
+
+sed '/^$/d' output.txt > robs_compiled_adlist.txt
+sleep 1
+
+
+#Update Github project
+
+git add *
+sleep 1
+
+git commit -m "update script"
+sleep 1
+
+git push
+sleep 1
