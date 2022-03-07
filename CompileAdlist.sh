fb42c7643920f6b3eebb03205aebfbca8905a8e4
RC62 <RC62@users.noreply.github.com>
2022-03-07 14:48:47 -0500

    x

--- CompileAdlist.sh
+++ CompileAdlist.sh
@@ -6,32 +6,32 @@ cd /home/pi/blocklist/
 
 # Compile adlist files
 hostlist-compiler -c configuration.json -o output.txt
-sleep 1
+sleep 3
 
 # Cleanup IPs
 sed -i ''"s/127.0.0.1//g" output.txt
-sleep 1
+sleep 3
 
 sed -i ''"s/0.0.0.0//g" output.txt
-sleep 1
+sleep 3
 
 # Cleanup comments - replace "!" with "#"
 sed -i ''"s/\!/\#/g" output.txt
-sleep 1
+sleep 3
 
 # Remove all empty lines
 
 sed '/^$/d' output.txt > robs_compiled_adlist.txt
-sleep 1
+sleep 5
 
 
 #Update Github project
 
-git add *
-sleep 1
+git add .
+sleep 3
 
 git commit -m "update script"
-sleep 1
+sleep 3
 
 git push
-sleep 1
+sleep 3
