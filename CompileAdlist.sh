877e547e16e9714caad2673488c3687e8e1568d3
RC62 <rob@rc62.net>
2022-03-22 19:31:02 -0400

    script

--- CompileAdlist.sh
+++ CompileAdlist.sh
@@ -14,32 +14,32 @@ sleep 1
 
 # Compile adlist files
 hostlist-compiler -c configuration.json -o output.txt
-sleep 2
+sleep 3
 
 # Cleanup IPs
-sed -i ''"s/127.0.0.1//g" output.txt
-sleep 2
+sed -i ''"s/127.0.0.1//g" output.txt > output1.txt
+sleep 3
 
-sed -i ''"s/0.0.0.0//g" output.txt
-sleep 2
+sed -i ''"s/0.0.0.0//g" output1.txt > output2.txt
+sleep 3
 
 # Cleanup comments - replace "!" with "#"
-sed -i ''"s/\!/\#/g" output.txt
-sleep 2
+sed -i ''"s/\!/\#/g" output2.txt > output3.txt
+sleep 3
 
 # Remove all empty lines
 
-sed '/^$/d' output.txt > robs_compiled_adlist.txt
-sleep 2
+sed '/^$/d' output3.txt > robs_compiled_adlist.txt
+sleep 3
 
 
 # Update Github project
 cd /home/pi/blocklist/
 git add .
-sleep 2
+sleep 3
 
 git commit -a -m "script" 
-sleep 2
+sleep 3
 
 git push
 
